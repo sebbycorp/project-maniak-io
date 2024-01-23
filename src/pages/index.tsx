fc0be400f7006e0d3e3e01c1adef3485f7852d39
dmitry-antonyuk <dmitry@antonyuk.org>
2024-01-23 16:54:34 +0200

    base hero setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { routes } from '@/constants/routes';
+import { Hero } from '@/sections/hero';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
 
 export default function Home() {
-  return <></>;
+  return <Hero />;
 }
---
+++ src/sections/hero.tsx
@@ -0,0 +1,3 @@
+export function Hero() {
+  return <div className="w-[340px] h-[800px] fill-[rgba(36, 99, 235, 0.8)] blur-xl"></div>;
+}
