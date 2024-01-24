1bb853b23a248c0a10d667a5c0e04df6db3b59b7
dmitry-antonyuk <dmitry@antonyuk.org>
2024-01-24 13:57:27 +0200

    trustedBy section

---
+++ src/constants/trusted-by.ts
@@ -0,0 +1,22 @@
+import vault from '/assets/trusted-by/vault.png';
+import paloalto from '/assets/trusted-by/paloalto.png';
+import fortinet from '/assets/trusted-by/fortinet.png';
+import consul from '/assets/trusted-by/consul.png';
+import terraform from '/assets/trusted-by/terraform.png';
+import redHat from '/assets/trusted-by/red-hat.png';
+import aws from '/assets/trusted-by/aws.png';
+import googleCloud from '/assets/trusted-by/google-cloud.png';
+import azure from '/assets/trusted-by/azure.png';
+
+export const trustedBy = [
+  vault,
+  paloalto,
+  fortinet,
+  consul,
+  terraform,
+  redHat,
+  aws,
+  googleCloud,
+  azure,
+  redHat,
+];
---
+++ src/sections/trusted-by/index.tsx
@@ -0,0 +1,25 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { trustedBy } from '@/constants/trusted-by';
+import Image from 'next/image';
+
+export function TrustedBy() {
+  return (
+    <div className="my-24">
+      <h2 className="text-4xl font-semibold mb-5">Devops&Cloud Playgrounds</h2>
+      <p className="text-xl text-[#7F8A99] tracking-[0.4px]">
+        Powerful, self-serve product and growth analytics to help you convert, engage, and retain
+        more users. Trusted by over 4,000 startups.
+      </p>
+      <div className="mt-12 grid grid-cols-5 gap-5">
+        {trustedBy.map((el, i) => (
+          <Card
+            className="flex justify-center items-center h-[175px] px-8 py-8 overflow-hidden"
+            key={i}
+          >
+            <Image src={el} alt="Card Image" className=" object-contain max-h-[140px]" />
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
