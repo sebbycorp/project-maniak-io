34c39a6512f7b14b6cbec8777fd462d271078310
dmitry-antonyuk <dmitry@antonyuk.org>
2024-01-30 15:30:16 +0200

    revert

--- src/sections/subscribe/index.tsx
+++ src/sections/subscribe/index.tsx
@@ -3,24 +3,26 @@ import { Input } from '@/components/ui/input';
 
 export function Subscribe() {
   return (
-    <div className="py-24 bg-[#F9FAFB] w-screen container">
-      <h3 className="text-4xl text-center text-[#101828] font-semibold">
-        Subscribe to my newsletter
-      </h3>
-      <p className="text-[#7F8A99] text-center font-medium mt-4 mb-8">
-        Read articles from Sebastian Maniak directly inside your inbox. Subscribe to the <br />
-        {` `}
-        newsletter, and don`t miss out.
-      </p>
-      <div className="mx-auto flex justify-center items-center gap-4 max-sm:flex-col">
-        <Input
-          className="w-[360px] h-[54px] text-base max-sm:w-full placeholder:text-base placeholder:tracking-[0.32px]"
-          type="email"
-          placeholder="Enter your email"
-        />
-        <Button className="bg-[#2463EB] px-6 h-[54px] max-sm:w-full text-normal font-semibold text-white">
-          Subscribe
-        </Button>
+    <div className="py-24 bg-[#F9FAFB] w-screen">
+      <div className="container">
+        <h3 className="text-4xl text-center text-[#101828] font-semibold">
+          Subscribe to my newsletter
+        </h3>
+        <p className="text-[#7F8A99] text-center font-medium mt-4 mb-8">
+          Read articles from Sebastian Maniak directly inside your inbox. Subscribe to the <br />
+          {` `}
+          newsletter, and don`t miss out.
+        </p>
+        <div className="mx-auto flex justify-center items-center gap-4 max-sm:flex-col">
+          <Input
+            className="w-[360px] h-[54px] text-base max-sm:w-full placeholder:text-base placeholder:tracking-[0.32px]"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <Button className="bg-[#2463EB] px-6 h-[54px] max-sm:w-full text-normal font-semibold text-white">
+            Subscribe
+          </Button>
+        </div>
       </div>
     </div>
   );
