e23143cafba066639d8168e99d12b5a91e8775f2
dmitry-antonyuk <dmitry@antonyuk.org>
2024-01-30 15:28:02 +0200

    responsive home page

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -8,10 +8,10 @@ import img from '/public/hero/hero.jpg';
 
 export function Hero() {
   return (
-    <div className="h-[800px] relative flex gap-[64px] items-center">
+    <div className="py-32 relative flex gap-[64px] items-center max-md:flex-col max-md:pt-12">
       <div>
         <Badge className="mb-2">Technology Evangelist | Advisor | Consultant</Badge>
-        <h2 className="text-[#101828] text-[48px] tracking-[0.96px] leading-[60px]">
+        <h2 className="text-[#101828] text-5xl tracking-[0.96px] text-balance leading-[60px] max-md:text-2">
           I build,secure and automate{' '}
           <span className="text-transparent  bg-clip-text bg-gradient-to-l from-[#2463EB] to-[#84C8F9]">
             infrastructure
@@ -26,7 +26,7 @@ export function Hero() {
         </Button>
       </div>
       <div>
-        <Image src={img} alt="Hero image" />
+        <Image src={img} alt="Hero image" className="rounded-2xl" />
       </div>
       <LinearGradient className="absolute bottom-0 left-1/2 -translate-x-1/2 -z-10" />
     </div>
